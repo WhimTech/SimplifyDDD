5e808d364f931a421d5a64583e1b2c1db39b4e62
CodeDogFish <jackcool1989@outlook.com>
2014-12-19 17:00:19 +0800

    Add Logging Block

---
+++ SimplifyDDD/Logging/ILoggerFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimplifyDDD.Logging
+{
+    /// <summary>Represents a logger factory.
+    /// </summary>
+    public interface ILoggerFactory
+    {
+        /// <summary>Create a logger with the given logger name.
+        /// </summary>
+        ILogger Create(string name);
+        /// <summary>Create a logger with the given type.
+        /// </summary>
+        ILogger Create(Type type);
+    }
+}
