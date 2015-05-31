b6ee39c33d1c30ac8ae992ee514d944cde8d1582
JackYu <codedogfish@github.com>
2015-05-31 16:57:43 +0800

    add configuration extension

---
+++ SimplifyDDD.Log4Net/ConfigurationExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Practices.Unity;
+using SimplifyDDD;
+using SimplifyDDD.Config;
+using SimplifyDDD.Logging;
+using SimplifyDDD.Repository;
+using SimplifyDDD.UnitOfWork;
+
+namespace SimplifyDDD.Log4Net
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ConfigurationExtension
+    {
+        public static SimplifyDDDConfiguration RegisterLog4Net(this SimplifyDDDConfiguration simplifyDddConfiguration)
+        {
+            IoCFactory.Instance.CurrentContainer.RegisterType<ILoggerFactory, Log4NetLoggerFactory>();
+            IoCFactory.Instance.CurrentContainer.RegisterType<ILogWriter, Log4NetLogWriter>();
+            return simplifyDddConfiguration;
+        }
+
+
+    }
+}
